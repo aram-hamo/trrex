c3d6a979c4bb65a11a573d623565bb2239dfe187
Aram Hamo <contact@aramhamo.me>
2023-06-13 11:14:17 +0200

    modified:   public/worker.js

--- public/worker.js
+++ public/worker.js
@@ -1,4 +1,25 @@
+const staticContent = 'static-assets-v0';
+const assets = ['/','/icon.png',
+'/index.html','/calc.js',
+'languages.json', 'manifest.json'
+];
+// installation: caching static assets
 self.addEventListener('install', evt=> {
+  console.log("install");
+  evt.waitUntil(
+  caches.open(staticContent).then(cache=>{
+    cache.addAll(assets);
+  }))
 });
+
+// deleting old cache
 self.addEventListener('activate', evt=> {
+  console.log("activate");
+async () => {
+  const cacheKeepList = staticContent;
+  const keyList = await caches.keys();
+  const cachesToDelete = keyList.filter((key) => !cacheKeepList.includes(key));
+  await Promise.all(cachesToDelete.map(deleteCache));
+};
+
 });
